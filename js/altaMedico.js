b1a1c20f1abafff87d6b6038b98000045630d29a
Facu+ <Faacuusb@hotmail.com>
2025-10-19 20:14:18 +0200

    alta medicos y guardado en localStorage

---
+++ js/altaMedico.js
@@ -0,0 +1,43 @@
+const altaMedicoFormulario = document.getElementById("altaMedicoFormulario");
+const inputNombre = document.getElementById("nombre");
+const inputEspecialidad = document.getElementById("especialidad");
+const inputObraSocial = document.getElementById("obrasocial");
+
+function obtenerProximoId() {
+  let ultimoId = parseInt(localStorage.getItem("ultimoIdMedico")) || 0;
+  ultimoId++;
+  localStorage.setItem("ultimoIdMedico", ultimoId);
+  return ultimoId;
+}
+
+function altaMedicos(event) {
+  event.preventDefault();
+
+  let nombreMed = inputNombre.value.trim();
+  let especialidad = inputEspecialidad.value.trim();
+  let obrasocial = inputObraSocial.value.trim();
+
+  if (!nombreMed || !especialidad || !obrasocial) {
+    alert("Por favor completa los campos!");
+    return;
+  }
+  const nuevoMed = {
+    id: obtenerProximoId(),
+    nombre: nombreMed,
+    especialidad: especialidad,
+    obrasocial: obrasocial,
+  };
+
+  const medicos = JSON.parse(localStorage.getItem("medicos")) || [];
+  medicos.push(nuevoMed);
+  localStorage.setItem("medicos", JSON.stringify(medicos));
+
+  alert(
+    `Medico registrado:\n\n` +
+      `Nombre: ${nombreMed}\n` +
+      `Especialidad: ${especialidad}\n` +
+      `Obra Social: ${obrasocial}\n`
+  );
+  altaMedicoFormulario.reset();
+}
+altaMedicoFormulario.addEventListener("submit", altaMedicos);
