25121e1449930b6603549e46251b389cfc093f5e
Facu+ <Faacuusb@hotmail.com>
2025-10-21 01:39:51 +0200

    fix bugs y render de cards en gestion de medicos, add btn, funcion de eliminar medico

--- js/altaMedico.js
+++ js/altaMedico.js
@@ -2,12 +2,38 @@ const altaMedicoFormulario = document.getElementById("altaMedicoFormulario");
 const inputNombre = document.getElementById("nombre");
 const inputEspecialidad = document.getElementById("especialidad");
 const inputObraSocial = document.getElementById("obrasocial");
+const inputImagen = document.getElementById('imagenMedico');
+const muestraImagen = document.getElementById('muestraImagen');
+
+let imagenBase64 = '';
+
+inputImagen.addEventListener('change', function(e){
+    const archivo = e.target.files[0];
+    if (archivo){
+        const lector = new FileReader();
+
+        lector.onload = function(e){
+            imagenBase64 = e.target.result;
+            muestraImagen.src = imagenBase64;
+            muestraImagen.style.display = 'block';
+        };
+        lector.readAsDataURL(archivo);
+    }
+})
 
 function obtenerProximoId() {
-  let ultimoId = parseInt(localStorage.getItem("ultimoIdMedico")) || 0;
-  ultimoId++;
-  localStorage.setItem("ultimoIdMedico", ultimoId);
-  return ultimoId;
+  let medicos = JSON.parse(localStorage.getItem("medicos")) || [];
+  if (medicos.length === 0){
+    return 1;
+  }
+  let ultimoId = 0;
+  for (let i = 0; i< medicos.length; i++){
+    if (medicos[i].id > ultimoId){
+      ultimoId = medicos[i].id;
+    }
+  }
+  return ultimoId + 1
+  
 }
 
 let medicos = JSON.parse(localStorage.getItem("medicos")) || [];
@@ -28,9 +54,9 @@ function altaMedicos(event) {
     nombre: nombreMed,
     especialidad: especialidad,
     obrasocial: obrasocial,
+    imagen:imagenBase64 || ''
   };
 
-  
   medicos.push(nuevoMed);
   localStorage.setItem("medicos", JSON.stringify(medicos));
 
@@ -41,5 +67,7 @@ function altaMedicos(event) {
       `Obra Social: ${obrasocial}\n`
   );
   altaMedicoFormulario.reset();
+  muestraImagen.style.display = 'none';
+  imagenBase64 = '';
 }
 altaMedicoFormulario.addEventListener("submit", altaMedicos);
